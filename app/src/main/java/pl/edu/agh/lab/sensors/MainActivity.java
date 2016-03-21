ae3543a336afcca1abe1c5f9d373f84d37f01478
karobale <vass.chan@gmail.com>
2016-03-21 15:18:45 +0100

    Correct directory structure

--- app/src/main/java/pl/edu/agh/lab/sensors/MainActivity.java
+++ app/src/main/java/pl/edu/agh/lab/sensors/MainActivity.java
@@ -13,7 +13,6 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ListView;
